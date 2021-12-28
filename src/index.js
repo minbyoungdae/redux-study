9410ac1d22f527993b981dd44939ee083031ebaa
minbyoungdae <80196999+minbyoungdae@users.noreply.github.com>
2021-12-28 19:01:13 +0900

    Fix: React 형식으로 변경

--- src/index.js
+++ src/index.js
@@ -1,40 +1,50 @@
-import { createStore } from "redux";
+// import { createStore } from "redux";
 
-const add = document.getElementById("add");
-const minus = document.getElementById("minus");
-const number = document.querySelector("span");
+// const add = document.getElementById("add");
+// const minus = document.getElementById("minus");
+// const number = document.querySelector("span");
 
-const countModifier = (count = 0, action) => {
-  switch (action.type) {
-    case "ADD":
-      return count + 1;
-    case "MINUS":
-      return count - 1;
-    default:
-      return count;
-  }
-};
-// reducer는 state를 변경하는 함수.
-// reducer에서 리턴하는 값이 어플리케이션의 데이터
-// 유일하게 state를 변경할 수 있는 곳
-// 매개변수 state = 0 은 기본값을 0으로 설정하는 문법
+// const countModifier = (count = 0, action) => {
+//   switch (action.type) {
+//     case "ADD":
+//       return count + 1;
+//     case "MINUS":
+//       return count - 1;
+//     default:
+//       return count;
+//   }
+// };
+// // reducer는 state를 변경하는 함수.
+// // reducer에서 리턴하는 값이 어플리케이션의 데이터
+// // 유일하게 state를 변경할 수 있는 곳
+// // 매개변수 state = 0 은 기본값을 0으로 설정하는 문법
 
-const countStore = createStore(countModifier);
-// 스토어는 state(값이 변하는 데이터)를 저장하는 장소
-// createStore를 사용하여 생성
-// createStore의 매개변수로 reducer가 들어와야 한다.
-// 이 때 reducer는 함수여야한다.
+// const countStore = createStore(countModifier);
+// // 스토어는 state(값이 변하는 데이터)를 저장하는 장소
+// // createStore를 사용하여 생성
+// // createStore의 매개변수로 reducer가 들어와야 한다.
+// // 이 때 reducer는 함수여야한다.
 
-// countStore.dispatch({ type: "ADD" });
-// dispatch를 통해 리듀서에 액션을 전달한다 ({ type: "ADD"})
-// getState를 통해 현재 state를 확인한다
+// // countStore.dispatch({ type: "ADD" });
+// // dispatch를 통해 리듀서에 액션을 전달한다 ({ type: "ADD"})
+// // getState를 통해 현재 state를 확인한다
 
-const onChange = () => {
-  number.innerText = countStore.getState();
-};
+// const onChange = () => {
+//   number.innerText = countStore.getState();
+// };
 
-countStore.subscribe(onChange);
-// store의 값이 바뀔때마다 매개변수를 실행
+// countStore.subscribe(onChange);
+// // store의 값이 바뀔때마다 매개변수를 실행
 
-add.addEventListener("click", () => countStore.dispatch({ type: "ADD" }));
-minus.addEventListener("click", () => countStore.dispatch({ type: "MINUS" }));
+import React from "react";
+import ReactDom from "react-dom";
+import React_App from "./React_App";
+import { Provider } from "react-redux";
+import store from "./store";
+
+ReactDom.render(
+  <Provider store={store}>
+    <React_App />
+  </Provider>,
+  document.getElementById("root")
+);
